ccf37cb37304d568b9363f9342e527824215ecf0
kumac13 <foohilack@gmail.com>
2022-05-22 16:50:00 +0900

    feat:

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-const sayHello = (name: string) => {
-  return `Hello, ${name}!`
-}
+import { Salesforce } from "./Salesforce";
+import { QueryBuilder } from "./QueryBuilder";
 
-console.log(sayHello('John'))
+(async () => {
+  // Alfredからinputを受け取る
+
+  // inputからQueryを作る
+  let queryBuilder = new QueryBuilder("sample");
+  let query = queryBuilder.call();
+
+
+  // SalesforceにQuueryを渡して結果を得る
+  let salesforce = new Salesforce();
+  await salesforce.set();
+  let result = await salesforce.call(query);
+
+  // Alfredに結果を表示
+  console.log(result);
+})();
