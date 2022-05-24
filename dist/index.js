d863f6eb7b4864f30741b669da5ce0b0493677be
kumac13 <foohilack@gmail.com>
2022-05-24 19:51:42 +0900

    feat: add alfy process

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,24 @@
 import { Salesforce } from "./Salesforce.js";
 import { QueryBuilder } from "./QueryBuilder.js";
+import alfy from "alfy";
 (async () => {
     // Alfredからinputを受け取る
-    // inputからQueryを作る
-    let queryBuilder = new QueryBuilder("sample");
-    let query = queryBuilder.call();
-    // SalesforceにQuueryを渡して結果を得る
-    let salesforce = new Salesforce();
-    await salesforce.set();
-    let result = await salesforce.call(query);
-    // Alfredに結果を表示
-    console.log(result);
+    if (alfy.input.length > 0) {
+        let queryBuilder = new QueryBuilder(alfy.input);
+        let query = queryBuilder.call();
+        // SalesforceにQueryを渡して結果を得る
+        let salesforce = new Salesforce();
+        await salesforce.set();
+        let result = await salesforce.call(query);
+        let object = "Account";
+        // Alfredに結果を表示
+        alfy.output(result.map((x) => ({
+            title: x.Name,
+            subtitle: x.Id,
+            arg: `https://teamspirit-2987.lightning.force.com/lightning/r/${object}/${x.Id}/view`,
+        })));
+    }
+    else {
+        alfy.output([{ title: "Waiting...", subtitle: "", arg: "" }]);
+    }
 })();
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
-import { Salesforce }   from "./Salesforce.js";
+import { Salesforce } from "./Salesforce.js";
 import { QueryBuilder } from "./QueryBuilder.js";
-import alfy from "alfy"
+import alfy from "alfy";
 
 (async () => {
-  // Alfredからinputを受け取る
+  if (alfy.input.length > 0) {
+    let queryBuilder = new QueryBuilder(alfy.input);
+    let query = queryBuilder.call();
 
-  // inputからQueryを作る
-  let queryBuilder = new QueryBuilder("sample");
-  let query = queryBuilder.call();
+    let salesforce = new Salesforce();
+    await salesforce.set();
+    let result = await salesforce.call(query);
+    let object = "Account";
 
-  // SalesforceにQuueryを渡して結果を得る
-  let salesforce = new Salesforce();
-  await salesforce.set();
-  let result = await salesforce.call(query);
-
-  // Alfredに結果を表示
-  console.log(result);
+    alfy.output(
+      result.map((x) => ({
+        title: x.Name,
+        subtitle: x.Id,
+        arg: `https://teamspirit-2987.lightning.force.com/lightning/r/${object}/${x.Id}/view`,
+      }))
+    );
+  } else {
+    alfy.output([{ title: "Waiting...", subtitle: "", arg: "" }]);
+  }
 })();
